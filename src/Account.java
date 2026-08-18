ca6a631969cb15c161240bd1051bfb1b02c4eb2d
MAC <mac124056@gmail.com>
2026-08-18 22:38:31 +0530

    Module 2 : Applied Deposit ,Withdraw & Getter Method .|Encapsulation.

--- src/Account.java
+++ src/Account.java
@@ -9,6 +9,34 @@ public class Account {
         this.accountBalance = accountBalance;
     }
 
+    // Deposit Money
+    public boolean deposit(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        accountBalance += amount;
+        return true;
+    }
+
+    // Withdraw money.
+    public boolean withdraw(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        if (amount > accountBalance) {
+            return false;
+        }
+        accountBalance -= amount; {
+            return true;
+        }
+
+    }
+    //Getter method : Read only window into accountBalance.
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
     @Override
     public String toString() {
         return "Account #" + accountNumber + " | Owner: " + owner + " | Balance: " + accountBalance;
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,25 @@ public class Main {
     public static void main(String[] args) {
         Account acc1 = new Account("Rahul", 1001, 5000);
         //acc1.accountBalance = 99999;
-        System.out.println(acc1);
+        System.out.println("Start : " + acc1);
+
+        // Testing Deposit Method
+
+        System.out.println("Deposit 500 -> Success?" + acc1.deposit(500));
+        System.out.println("After :" + acc1);
+
+        System.out.println("Deposit -500 -> Success?" + acc1.deposit(-500));
+        System.out.println("After :" + acc1);
+
+        //Testing Withdraw Method
+
+        System.out.println("Withdraw 2000 -> success? " + acc1.withdraw(2000));
+        System.out.println("After: " + acc1);
+
+        System.out.println("Withdraw 999999 -> success? " + acc1.withdraw(999999));
+        System.out.println("After: " + acc1);
+
+        // Testing Getter Method
+        System.out.println("Balance via getter: " + acc1.getAccountBalance());
     }
 }
